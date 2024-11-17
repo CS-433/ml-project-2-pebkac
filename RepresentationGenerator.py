e0cc0861fe86a703609fd92c0ace286fc3447577
tjobin <timothe.jobin@epfl.ch>
2024-11-17 16:59:31 +0100

    add comments on RepresentationGenerator.py with two question marks still

--- RepresentationGenerator.py
+++ RepresentationGenerator.py
@@ -1,18 +1,11 @@
 import os
 import numpy as np
 import qml2
-import matplotlib.pyplot as plt
-import glob
 from qml2.representations.slatm import get_slatm_mbtypes, generate_slatm
 from qml2.representations.cMBDF import generate_cmbdf, get_asize, get_convolutions
-from rdkit.Chem import MolFromSmiles, AddHs
-from rdkit.Chem import rdFingerprintGenerator
+from rdkit.Chem import MolFromSmiles, AddHs, rdFingerprintGenerator
 from qstack import compound, spahm
 from tqdm import tqdm
-from rdkit import RDLogger
-
-# Enable RDKit logging for debugging
-RDLogger.EnableLog('rdApp.error')
 
 
 class RepresentationGenerator:
@@ -57,66 +50,88 @@ class RepresentationGenerator:
         }
     
     def OneHotRepresentation(self, input_dir):
+        # Retrieves the number of files (molecules)
         N = len(os.listdir(input_dir))
-        labels = np.arange(0, N, 1)
-        unique_labels, indices = np.unique(labels, return_inverse=True)
-        one_hot = np.eye(len(unique_labels))[indices]
+        # Generates the one-hot representation matrix for all amino acids
+        one_hot = np.eye(N)
         return one_hot
 
     def cMBDFRepresentation(self, input_dir):
         molsList = []
+
         for fileName in os.listdir(input_dir):
+
+            # converts the amino acid contained in each .xyz file to a qml2.Compound object
             mol = qml2.Compound(input_dir + "/" + fileName)
             
+            # recovers the number of atoms in each amino acid
             with open(input_dir + "/" + fileName, 'r') as file:
                 lines = file.read().split('\n')
                 numAtoms = int(lines[0])
             
+            # concatenates the amino acid and its number of atoms in a list
             subList = [mol, numAtoms]
             molsList.append(subList)
 
+        # stores the qml2.Compound and the numbers of atoms in two lists
         compoundsList = [subList[0] for subList in molsList]
         sizesList = [subList[1] for subList in molsList]
 
+        # extracts the charges and coordinates of each amino acid from the qml2.Compound objects
         charges = [compound.nuclear_charges for compound in compoundsList]
         coordinates = [compound.coordinates for compound in compoundsList]
         pad = max(sizesList)
 
+        # not sure what that does ?
         asize = get_asize(charges)
         convs = get_convolutions()
         repsList = []
+
+        # generates the cMBDF representation of each amino acid and stores it in a list
         print("Generating cMBDF representations: ")
         for i in tqdm(range(len(compoundsList))):
             rep = generate_cmbdf(charges[i], coordinates[i], convs, local=False, pad=pad, asize=asize)
             repsList.append(rep)
 
+        # stacks the representation of each amino acid as a row in a np.array
         A = np.row_stack(tuple(repsList))
         return A
 
     def SLATMRepresentation(self, input_dir):
         molsList = []
+
         for fileName in os.listdir(input_dir):
+
+            # converts the amino acid contained in each .xyz file to a qml2.Compound objec
             mol = qml2.Compound(input_dir + "/" + fileName)
             
+            # recovers the number of atoms in each amino acid
             with open(input_dir + "/" + fileName, 'r') as file:
                 lines = file.read().split('\n')
                 numAtoms = int(lines[0])
             
+            # concatenates the amino acid and its number of atoms in a list
             subList = [mol, numAtoms]
             molsList.append(subList)
 
+        # stores the qml2.Compound and the numbers of atoms in two lists
         compoundsList = [subList[0] for subList in molsList]
 
+        # extracts the charges and coordinates of each amino acid from the qml2.Compound objects
         charges = [compound.nuclear_charges for compound in compoundsList]
         coordinates = [compound.coordinates for compound in compoundsList]
 
+        # not sure what that does ?
         mbtypes = get_slatm_mbtypes(charges)
         repsList = []
+
+        # generates the SLATM representation of each amino acid and stores it in a list
         print("Generating SLATM representations: ")
         for i in tqdm(range(len(compoundsList))):
             rep = generate_slatm(charges[i], coordinates[i], mbtypes)
             repsList.append(rep)
 
+        # stacks the representation of each amino acid as a row in a np.array
         A = np.row_stack(tuple(repsList))
         return A
     
@@ -125,13 +140,20 @@ class RepresentationGenerator:
         
         print("Generating SPAHM representations: ")
         for fileName in tqdm(os.listdir(input_dir)):
+
+            # converts the amino acid contained in each .xyz file to a rdkit.Chem.rdchem.Mol object
             mol = compound.xyz_to_mol(input_dir + '/' + fileName, 'def2-svp', charge=0, spin=0)
+
+            # generates the SPAHM represenation of the amino acid and stores it in a list
             rep = spahm.compute_spahm.get_spahm_representation(mol, "lb")[0, :]
             reps_list.append((rep, rep.size))
         
+        # retrieves the longest representation
         max_size = max(reps_list, key=lambda t: t[1])[1]
         padded_reps_list = []
 
+        # concatenates each representation vector and an all-zero np.array to make all 
+        # representations of equal length and stores them in a list
         for rep, rep_size in reps_list:
             zeros = np.zeros(max_size - rep_size)
             padded_rep = np.concatenate((rep, zeros), axis=None)
@@ -143,14 +165,20 @@ class RepresentationGenerator:
     
     def MorganFingerprints(self, input_dir):
         molsList = []
+
+        # converts the amino acid contained in each .xyz file to a rdkit.Chem.rdchem.Mol object
+        # via its smiles description
         print("Generating Morgan fingerprints: ")
         for fileName in tqdm(os.listdir(input_dir)):
             mol = MolFromSmiles(self.aa_smiles[fileName[0]])
             mol = AddHs(mol)
             molsList.append(mol)
 
+        # initializes a Morgan Fingerprint generator
         fpgen = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=128)
         fpList = []
+
+        # generates the Morgan FP representation of each amino acid and stores it in a list
         fpList = list(map(fpgen.GetFingerprint, molsList))
 
         A = np.row_stack(tuple(fpList))
@@ -158,19 +186,19 @@ class RepresentationGenerator:
     
     
     def RepresentationGeneration(self, input_dir):
+
+        # Generates the desired representation
         A = self.repDict[self.repType](input_dir)
         return A
     
 
 def RepsNormalization(A):
     """
-    Commentaire de Jacques: 21 dans ce cas ? Il ne faut pas normaliser la représentation one-hot du gap :)
-    Aussi, il faut préciser que c'est une layer normalisation (par représentation et non pas par feature)
-    Normalizes the matrix of representation A
+    Normalizes each row (layer-normalization) of the representation matrix A
     inputs:
-        A : np.array of shape (21, D)
+        A : np.array of shape (20, D)
     returns:
-        A_normalized : np.array of shape (21, D)
+        A_normalized : np.array of shape (20, D)
     """
     row_avg = A.mean(axis=1)
     row_std = A.std(axis=1)
@@ -182,16 +210,23 @@ def RepsNormalization(A):
 
 def RemoveRedundantFeatures(A, threshold=0.9):
     """
-    Commentaire de Jacques: 21 ?
     Removes redundant features of a data set
     inputs:
-        A : np.array of shape (21, D)
+        A : np.array of shape (20, D)
     returns:
-        A_noredundant : np.array of shape (21, D_new)
+        A_noredundant : np.array of shape (20, D_new)
     """
+
+    # calculates the correlation coefficient matrix
     corrMat = np.corrcoef(A.T)**2
+
+    # retrieves only the lower triangular part of the symmetric matrix
     lowerTriangular = np.tril(corrMat, k=-1)
+
+    # retrieves one index from all pairs of redundant features
     redundantIndices = np.unique(np.nonzero(lowerTriangular > threshold)[1])
+
+    # deletes one feature from each pair of redundant feature
     A_noredundant = np.delete(A, redundantIndices, axis=1)
 
     return A_noredundant
@@ -209,15 +244,22 @@ def GenerateRepresentation(rep_name, input_dir):
     reps = RepresentationGenerator(rep_name).RepresentationGeneration(input_dir)
 
     if rep_name in ["SPAHM", "SLATM", "cMBDF"]:
+
+        # normalizes the rep. matrix if rep. is "SPAHM", "SLATM" or "cMBDF"
         reps_cleaned = RepsNormalization(reps)
     if rep_name in ["SLATM", "cMBDF"]:
+
+        # removes redundant features from rep. matrix if rep. is "SLATM" or "cMBDF"
         reps_cleaned = RemoveRedundantFeatures(reps_cleaned)
     else:
         reps_cleaned = reps
 
+    # retrieves the dimensions of the cleaned rep. matrix
     N = np.shape(reps_cleaned)[0]
     D_new = np.shape(reps_cleaned)[1]
 
+    # adds a row that represents the "gap" and a feature that takes values 0 for 
+    # all amino acids and 1 for the "gap"
     reps_cleaned_with_gap = np.zeros((N+1, D_new+1))
     reps_cleaned_with_gap[:N, :D_new] = reps_cleaned
     reps_cleaned_with_gap[-1, -1] = 1
