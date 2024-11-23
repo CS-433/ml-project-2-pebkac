c13b72f9e8a3d7cc012ce086177dab9a7a5e8b65
tjobin <timothe.jobin@epfl.ch>
2024-11-23 16:50:42 +0100

    addition of file sdf_to_xyz.py

---
+++ sdf_to_xyz.py
@@ -0,0 +1,45 @@
+import os
+import glob
+from rdkit import Chem
+
+
+aa_onelettercode = ["A",
+                "C",
+                "D",
+                "E",
+                "F",
+                "G",
+                "H",
+                "I",
+                "K",
+                "L",
+                "M",
+                "N",
+                "P",
+                "Q",
+                "R",
+                "S",
+                "T",
+                "V",
+                "W",
+                "Y"]
+
+def sdf_to_xyz(input_dir, output_dir):
+    """
+    Converts .sdf files in .xyz files and stores them in a new directory
+    Important : each file contains only one molecule
+    inputs :
+        input_dir : str, name of directory where .sdf files are stored
+        output_dir : str, name of directory where .xyz files will be stored
+    returns :
+        
+    """
+    if output_dir not in os.listdir("."):
+        os.mkdir(output_dir)
+    sdf_files = glob.glob(input_dir + '/*.sdf')
+
+    for i, sdf_file in enumerate(sdf_files):
+        suppl = Chem.rdmolfiles.ForwardSDMolSupplier(sdf_file, removeHs=False)
+        label = aa_onelettercode[i]
+        for mol in suppl:
+            Chem.rdmolfiles.MolToXYZFile(mol, output_dir + "/" + label + ".xyz")
