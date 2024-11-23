ef2421a567d3030864961359da665ef479cfe73d
GregoirePachebat <114913256+GregoirePachebat@users.noreply.github.com>
2024-11-23 16:25:58 +0100

    implementation of validation

--- train.py
+++ train.py
@@ -13,14 +13,17 @@ def train_model(model, list_ids, file_path_training, file_path_validation, num_e
     # Loads the data
     training_set = Dataset(list_ids, file_path_training)
 
-    # Generates the batches
+    # Generates the batches for the training
     training_generator = torch.utils.data.Dataloader(training_set, **params)
 
+    # Generates the batches for the cross validation
+    ### Not necessary if batches already made. Struggling to understand where the batches should be generated
     validation_set = Dataset(list_ids, file_path_validation)
     validation_generator = torch.utils.data.DataLoader(validation_set, **params)
 
 
     # Sets the adam optimizer
+    ### I don't know to what lr should be set
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     for epoch in range(num_epoch):
@@ -31,8 +34,7 @@ def train_model(model, list_ids, file_path_training, file_path_validation, num_e
 
             optimizer.zero_grad()
 
-            # Forward pass
-            outputs = model(local_batch)
+            # I took the forward as not useful
 
             # Compute loss
             loss = model.loss(local_batch, local_labels) ## problem should be local weights but i am not sure about dataloader work
@@ -48,9 +50,12 @@ def train_model(model, list_ids, file_path_training, file_path_validation, num_e
 
         with torch.set_grad_enabled(False):
             for local_batch, local_labels in validation_generator:
+                validation_loss = 0.0
             # Transfer to GPU
                 local_batch, local_labels = local_batch.to(device), local_labels.to(device)
 
                 # Model computations
-                [...]
-    return model
+                loss = model.loss(local_batch, local_labels)
+
+                validation_loss += loss
+    return model, validation_loss, running_loss
