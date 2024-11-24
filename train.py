e35b6667d969facb01c867d146e368edfc91eb4e
GregoirePachebat <114913256+GregoirePachebat@users.noreply.github.com>
2024-11-24 12:12:27 +0100

    comments on train

--- train.py
+++ train.py
@@ -11,6 +11,25 @@ params = {'batch_size': 1,
 """
 
 def train_model(model, list_ids, file_path_training, file_path_validation, num_epoch, lr, verbose, **params):
+    """Implement a boucle d'entrainement. The model is sould be a transformer based on the one implemented in this code. 
+    parameters :
+        model : Transformer, the model we are training
+        list_ids : dict, mostly in our case only one ids as the model is trained on only one MSA
+        file_path_training : string, the path file. Has to be tailored for each machine
+        file_path_validation : string, the path file.
+        num_epoch : int, the number of iteration for the process
+        lr : float, the learning rate for the adam optimizer
+        verbose : bool, should the validation loss and running loss should be displayed. 
+        params : dict, the pârameters for the dataloader function. looks like 
+            params = {'batch_size': 1,
+            'shuffle': True,
+            'num_workers': 6}
+    
+    return : 
+        model : Transformer, the transformer trained
+        validation_loss_list : list, the list of all validation losses
+        running_loss_list : list, the list of all the running losses
+    """
 
     # CUDA for PyTorch
     use_cuda = torch.cuda.is_available()
@@ -18,7 +37,7 @@ def train_model(model, list_ids, file_path_training, file_path_validation, num_e
     """A quoi sert cette ligne ?
     Réponse :
     Si vos tailles d'entrée (par ex. taille des batches) ne changent pas fréquemment au cours de l'entraînement, activer cudnn.benchmark = True permet à cuDNN de rechercher automatiquement la méthode de calcul la plus rapide et de la réutiliser pour optimiser les performances.
-Cela est particulièrement utile pour des modèles avec des tailles d'entrée fixes, comme dans des images de taille constante ou des séquences de longueurs identiques."""
+    Cela est particulièrement utile pour des modèles avec des tailles d'entrée fixes, comme dans des images de taille constante ou des séquences de longueurs identiques."""
     torch.backends.cudnn.benchmark = True
     
     # Loads the data
@@ -38,6 +57,9 @@ Cela est particulièrement utile pour des modèles avec des tailles d'entrée fi
     # Sets the adam optimizer
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
+    running_loss_list = []
+    validation_loss_list = []
+
     for epoch in range(num_epoch):
         model.train()
         """A quoi sert running loss ?
@@ -53,8 +75,7 @@ Cela est particulièrement utile pour des modèles avec des tailles d'entrée fi
             optimizer.zero_grad()
 
             # Compute loss
-            """Si tu ne sais pas comment dataloader fonctionne, le mieux c'est de voir des exemples sur internet ou de tester ton code"""
-            loss = model.loss(local_batch, local_weights) ## problem should be local weights but i am not sure about dataloader work
+            loss = model.loss(local_batch, local_weights) 
 
             # Backward pass
             loss.backward()
@@ -63,7 +84,7 @@ Cela est particulièrement utile pour des modèles avec des tailles d'entrée fi
             optimizer.step()
 
             running_loss += loss.item()
-
+    
 
         model.eval()
         with torch.no_grad():
@@ -80,7 +101,10 @@ Cela est particulièrement utile pour des modèles avec des tailles d'entrée fi
         running_loss /= len(training_generator)
         validation_loss /= len(validation_generator)
 
+        running_loss_list.append(running_loss)
+        validation_loss_list.append(validation_loss)
+
         if verbose:
             tqdm.write(f"Epoch {epoch+1}: Training Loss = {running_loss:.4f}, Validation Loss = {validation_loss:.4f}")
 
-    return model, validation_loss, running_loss
+    return model, validation_loss_list, running_loss_list
