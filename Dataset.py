7e0d8866cd021053204478854d214054793f2be6
GregoirePachebat <114913256+GregoirePachebat@users.noreply.github.com>
2024-11-25 08:34:13 +0100

    refactor Dataset and train functions to use data dictionary and fold number

--- Dataset.py
+++ Dataset.py
@@ -5,19 +5,32 @@
 import torch
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, list_ids, file_path, weights):
-        ### Faut il ajouter le type de dataset qu'on cree ex 
-        self.list_ids = list_ids
-        self.weights = weights
-        self.file_path = file_path
+    def __init__(self, data_dict, fold_num, train_or_test):
+        """
+        Initializes the dataset with the specified fold and set type.
 
+        Parameters:
+        - data_dict (dict): The data dictionary containing the folds
+        - fold_num (int): The fold number to use (1-based index)
+        - train_or_test (str): Whether to use the 'Train' or 'Test' set
+        """
+        self.Z = data_dict[str(fold_num)][train_or_test]["Z"]  # Alignment matrix
+        self.W = data_dict[str(fold_num)][train_or_test]["W"]  # Weight vector
 
     def __len__(self):
-        return len(self.list_ids)
+        """
+        Returns the number of sequences in the dataset.
+        """
+        return len(self.Z)
     
-    def __getitem__(self, index):
-        id = self.list_ids[index]
-        X = torch.load(self.file_path + id + ".pt")
-        weight = self.weights[id]
+    def __getitem__(self, idx):
+        """
+        Returns the sequence and its corresponding weight for a given index.
 
-        return X, weight
+        Parameters:
+        - idx (int): Index of the sequence to retrieve
+
+        Returns:
+        - tuple: (sequence, weight) at the specified index
+        """
+        return self.Z[idx], self.W[idx]
